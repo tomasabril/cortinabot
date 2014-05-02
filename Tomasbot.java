c9565cb427f5449ca9d5043a6621d8dae718c288
tomas <tomasabril@users.noreply.github.com>
2014-05-02 18:29:44 -0300

    targeting
    adicionado targeting pensando que o inimigo anda em linha reta com
    velocidade constante
    Parece que nao funciona

--- Tomasbot.java
+++ Tomasbot.java
@@ -5,9 +5,9 @@ import java.awt.Color;
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
- * Tomasbot - a robot by (your name here)
+ * Tomasbot - a robot
  */
-public class Tomasbot extends Robot
+public class Tomasbot extends AdvancedRobot
 {
 	/**
 	 * run: Tomasbot's default behavior
@@ -15,47 +15,55 @@ public class Tomasbot extends Robot
 	public void run() {
 		// Initialization of the robot should be put here
 
-		setColors(Color.green,Color.red,Color.green); // body,gun,radar
+		setColors(Color.red,Color.white,Color.green); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
+
 			ahead(120);
 			turnRadarRight(360);
 			back(120);
-
-		//	turnLeft(90);
-		//	ahead(100);
 			
 		}
 	}
 	
 	public void onScannedRobot(ScannedRobotEvent e) {
 
-		turnGunRight(normalize( getHeading() - getGunHeading() + e.getBearing() ));
-		fire( Math.min(400 / e.getDistance(), 3) );
-		//ahead(30);
+		// calculate firepower based on distance
+		double firePower = Math.min(500 / e.getDistance(), 3);
+		// calculate speed of bullet
+		double bulletSpeed = 20 - firePower * 3;
+
+		double absoluteBearing = getHeading() + e.getBearing();
+		double eLateralVelocity = e.getVelocity() * Math.sin(e.getHeading() - absoluteBearing);
+		double offset = Math.asin(eLateralVelocity/bulletSpeed);
+		turnGunRight(normalize( absoluteBearing - getGunHeading() + offset ));
+
+		//mira simples
+		//turnGunRight(normalize( getHeading() - getGunHeading() + e.getBearing() ));
+
+		// if the gun is cool and we're pointed at the target, shoot!
+		if ( getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 10 ) {
+			fire(firePower);
+		}
+
 	}
 
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		//back(10);
 		turnRight(normalize( e.getBearing() + 90 ));
-		//turnLeft(90);
-		//ahead(40);
 	}
 
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
 		back(20);
 		turnRight(90);
 		ahead(150);
-
 	}
+	
 	// normalizes a bearing to between +180 and -180
 	double normalize(double angle) {
 		while (angle >  180) angle -= 360;
 		while (angle < -180) angle += 360;
 		return angle;
 	}
+	
 }
