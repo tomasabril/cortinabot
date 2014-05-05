6cb4d4dd134be0e9967f74c5ccd7c65c60a2b1e8
tomas <tomasabril@users.noreply.github.com>
2014-05-04 21:11:54 -0300

    fix targeting
    changed angles to radians and now linear targeting works.
    100% win against sampleWallRobot

--- Tomasbot.java
+++ Tomasbot.java
@@ -1,5 +1,6 @@
 package cortinabot;
 import robocode.*;
+import robocode.util.Utils;
 import java.awt.Color;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
@@ -15,42 +16,49 @@ public class Tomasbot extends AdvancedRobot
 	public void run() {
 		// Initialization of the robot should be put here
 
-		setColors(Color.red,Color.white,Color.green); // body,gun,radar
+		setColors(Color.blue,Color.white,Color.green); // body,gun,radar
+
+		setAdjustRadarForRobotTurn(true);
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
 
 		// Robot main loop
 		while(true) {
 
-			ahead(120);
-			turnRadarRight(360);
+			setAhead(120);
+			setTurnRadarRight(360);
+			scan();
+			//execute();
 			back(120);
 			
 		}
 	}
 	
 	public void onScannedRobot(ScannedRobotEvent e) {
-
 		// calculate firepower based on distance
 		double firePower = Math.min(500 / e.getDistance(), 3);
-		// calculate speed of bullet
 		double bulletSpeed = 20 - firePower * 3;
 
-		double absoluteBearing = getHeading() + e.getBearing();
-		double eLateralVelocity = e.getVelocity() * Math.sin(e.getHeading() - absoluteBearing);
+		double eAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
+		double eLateralVelocity = e.getVelocity() * Math.sin(e.getHeadingRadians() - eAbsoluteBearing);
 		double offset = Math.asin(eLateralVelocity/bulletSpeed);
-		turnGunRight(normalize( absoluteBearing - getGunHeading() + offset ));
+		setTurnGunRightRadians(Utils.normalRelativeAngle( eAbsoluteBearing - getGunHeadingRadians() + offset ));
 
 		//mira simples
 		//turnGunRight(normalize( getHeading() - getGunHeading() + e.getBearing() ));
 
 		// if the gun is cool and we're pointed at the target, shoot!
 		if ( getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 10 ) {
-			fire(firePower);
+			setFire(firePower);
 		}
+		execute();
+			
+		turnRadarRightRadians(Utils.normalRelativeAngle( getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians() ));
 
 	}
 
 	public void onHitByBullet(HitByBulletEvent e) {
-		turnRight(normalize( e.getBearing() + 90 ));
+		setTurnRight(normalize( e.getBearing() + 90 ));
 	}
 
 	public void onHitWall(HitWallEvent e) {
@@ -61,8 +69,8 @@ public class Tomasbot extends AdvancedRobot
 	
 	// normalizes a bearing to between +180 and -180
 	double normalize(double angle) {
-		while (angle >  180) angle -= 360;
-		while (angle < -180) angle += 360;
+		while (angle >  180){ angle -= 360;}
+		while (angle < -180){ angle += 360;}
 		return angle;
 	}
 	
